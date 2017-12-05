82d157c480357af6a1c065ac21a6cde7538123ab
Stefan Merkel <merkel.s.ulm@gmail.com>
2017-12-05 12:29:37 +0100

    update

---
+++ src/Asd.Infra.Data/UoW/AsdUnitOfWork.cs
@@ -0,0 +1,28 @@
+﻿namespace Asd.Infra.Data.UoW
+{
+    using Asd.Domain.Core.Commands;
+    using Asd.Domain.Interfaces;
+    using Asd.Infra.Data.Context;
+    using System;
+
+    public class AsdUnitOfWork : IAsdUnitOfWork
+    {
+        private readonly AsdSqlContext _context;
+
+        public AsdUnitOfWork(AsdSqlContext context)
+        {
+            _context = context ?? throw new ArgumentNullException(nameof(context));
+        }
+
+        public AsdCommandResponse Commit()
+        {
+            return _context.SaveChanges() > 0 ? AsdCommandResponse.Ok : AsdCommandResponse.Fail;
+        }
+
+        public void Dispose()
+        {
+            _context.Dispose();
+            GC.SuppressFinalize(this);
+        }
+    }
+}
